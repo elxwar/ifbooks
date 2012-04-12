6a27622445e86891e36a0a4832531e5c48517e40
Eric Waring <ericw@the-feel.co.uk>
2012-04-12 12:25:17 +0100

    Created application using Refinery CMS 2.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ifbooks::Application.config.secret_token = '2a5b8f7dddddf990027374291f38572b3db650db00988ab4f1a9a0ddf4075fe5b933c46445462b7628a0cad5bbb41e6d08324a4cd28a35d8825bb0ca7b59d51e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ifbooks::Application.config.session_store :cookie_store, key: '_ifbooks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ifbooks::Application.config.session_store :active_record_store
