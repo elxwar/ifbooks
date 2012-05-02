24866b10ac180ec59f3a4846467efeccd2d920aa
Eric Waring <ericw@the-feel.co.uk>
2012-05-02 12:42:43 +0100

    after rollback and reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120427153801) do
+ActiveRecord::Schema.define(:version => 20120501104450) do
 
   create_table "refinery_books", :force => true do |t|
     t.string   "title"
