5c03f8189a9ac13a1e9270311e7629356dc05125
Eric Waring <ericw@the-feel.co.uk>
2012-09-21 17:16:26 +0100

    files migration

---
+++ db/migrate/20120921122836_add_files_to_books.rb
@@ -0,0 +1,16 @@
+class AddFilesToBooks < ActiveRecord::Migration
+  def self.up
+    1.upto(10) do |i|
+      @file_name = 'file_' + i.to_s + '_id'
+      add_column :refinery_books, @file_name, :integer
+    end
+
+  end
+
+  def self.down
+    1.upto(10) do |i|
+      @file_name = 'file_' + i.to_s + '_id'
+      remove_column :refinery_books, @file_name
+    end
+  end
+end
